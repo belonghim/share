f58a9db386618c14c6485d153b9facb1e47d9bfc
root <root@gps07.redhat.lab>
2024-11-18 20:46:23 +0900

    3

--- test/mzc/upgrade/hub.sh
+++
@@ -1,5 +0,0 @@
-export MRR=release-41611-41703
-export REGISTRY=registry.woorifg.lab:5000
-
-sh update-service.tmpl
-sh hub-binding.tmpl
---
+++ test/upgrade-script/hub.sh
@@ -0,0 +1,5 @@
+export MRR=ocp414-416
+export REGISTRY=gps07.redhat.lab:8443
+
+sh update-service.tmpl
+sh hub-binding.tmpl
