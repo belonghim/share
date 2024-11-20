d0d4b6b7816a56a9cdbbb4358ad9c6aeb5a11a66
root <root@gps07.redhat.lab>
2024-11-20 17:59:03 +0900

    0

--- test/upgrade-script/clusters.sh
+++ test/upgrade-script/clusters.sh
@@ -1,7 +1,7 @@
 export SIGNATURE=/data01/temp/mirror/ocp414-416/result/release-signatures/
-export REPO=ocp4
+export REPO=ocp414-416
 
 sh signature.tmpl
 sh cv-upstream.tmpl
-sh all-binding.tmpl
-cat placement-all.yaml
+sh clusters-binding.tmpl
+sh clusters-placement.tmpl
