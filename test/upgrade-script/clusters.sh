f777b33d0d3f80dddfdc121bf3ccd3e3d06ff6e4
root <root@gps03.redhat.lab>
2024-11-21 22:58:02 +0900

    4

--- test/upgrade-script/clusters.sh
+++ test/upgrade-script/clusters.sh
@@ -1,7 +1,5 @@
-export SIGNATURE=/data01/temp/mirror/ocp414-416/result/release-signatures/
 export REPO=release
 
-#sh signature.tmpl
 sh cv-upstream.tmpl
 sh clusters-binding.tmpl
 sh clusters-placement.tmpl
