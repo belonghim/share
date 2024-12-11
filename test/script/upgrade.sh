a43c2aa283e4eaf4b1309623560d5d50c8dc2b32
belonghim <singjc@hanmail.net>
2024-12-11 00:38:38 +0000

    1

--- test/script/upgrade.sh
+++ test/script/upgrade.sh
@@ -1,6 +1,6 @@
-CLUSTER=$1
-VERSION=4.15.35
-
+if [ "$CLUSTER" = "" ];then CLUSTER=$1;fi
+if [ "$VERSION" = "" ];then VERSION=$2;fi
+if [ "$CLUSTER" = "" -o "$VERSION" = "" ];then echo "Example: upgrade.sh <cluster> <Version>";exit 1;fi
 cat <<EOF
 apiVersion: cluster.open-cluster-management.io/v1beta1
 kind: ClusterCurator
@@ -12,4 +12,3 @@ spec:
   upgrade:
     desiredUpdate: $VERSION
 EOF
-
