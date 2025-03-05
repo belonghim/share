90d85e0820ec0abc71cf8e9d35313d1b1b2a6a46
root <root@gps04.redhat.lab>
2025-03-05 13:00:54 +0000

    2

--- policies/script/upgrade.sh
+++ policies/script/upgrade.sh
@@ -2,7 +2,7 @@ if [ "$CLUSTER" = "" ];then CLUSTER=$1;fi
 if [ "$VERSION" = "" ];then VERSION=$2;fi
 if [ "$CLUSTER" = "" -o "$VERSION" = "" ];then echo "Example: upgrade.sh <cluster> <Version>";exit 1;fi
 
-oc label mcl $CLUSTER policies.undeploy=
+#oc label mcl $CLUSTER policies.undeploy= 2>/dev/null
 
 cat <<EOF
 apiVersion: cluster.open-cluster-management.io/v1beta1
